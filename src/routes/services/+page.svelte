774ebf392c95d0853a4e155542e9e542a4929678
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-06-27 08:49:25 +0530

    added input chips for search

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -4,12 +4,21 @@
 	import ServiceCard from '$lib/service_card.svelte';
 	import Preloader from '$lib/Preloader.svelte';
 
+	import { page } from '$app/stores';
+
+	$: console.log($page.url.searchParams.get('search'));
+
 	export let data: PageData;
 </script>
 
 <Preloader>
 	<form method="get">
 		<div class="flex w-1/3 gap-2 ml-auto mt-4 mr-10">
+			{#if $page.url.searchParams.get('search')}
+				<p class="font-poppins mx-4 my-auto  variant-filled-surface rounded-md px-3 py-2">
+					{$page.url.searchParams.get('search')}
+				</p>
+			{/if}
 			<input class="input px-3" type="text" placeholder="Search..." name="search" />
 			<button type="submit" class="btn variant-filled-primary">Search</button>
 			<a href="/services" class="btn variant-filled-secondary">Clear</a>
